d4623b1e08284f5d568f6fc16fc9403c60c980fb
jod431047 <jod431047@gmail.com>
2023-07-11 21:28:58 +0200

    add product list and detail

---
+++ products/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
+from taggit.managers import TaggableManager
+from django.utils.translation import gettext_lazy as _
+from django.utils.text import slugify
+
+
+FLAG_TYPES =(
+    ('New','New'),
+    ('Sale','Sale'),
+    ('Feature','Feature'),
+)
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(_('name'),max_length=120)
+    description = models.TextField(_('description'),max_length=30000)
+    sku = models.IntegerField(_('sku'))
+    price = models.FloatField(_('price'))
+    subtitle = models.TextField(_('subtitle'),max_length=600)            # brand = models.""
+    image = models.ImageField(_('image'),upload_to='product')
+    brand = models.ForeignKey('Brand',verbose_name=('brand') , on_delete=models.CASCADE,related_name='product_brand')
+    flag = models.CharField(_('flag'),max_length=20,choices=FLAG_TYPES)
+    tags = TaggableManager()
+    slug = models.SlugField(null=True,blank=True)
+    
+    def __str__(self):
+        return f"{self.name} - {self.price}"
+    
+    def save(self , *args ,**kwargs): 
+        self.slug = slugify(self.name ) 
+        super(Product, self).save( *args ,**kwargs)
+
+
+class ProductImages(models.Model):
+    product = models.ForeignKey(Product,verbose_name=('product'),related_name='product_images',on_delete=models.CASCADE)
+    image = models.ImageField(_('image'),upload_to='product_images')
+
+
+class Brand(models.Model):
+    name = models.CharField(_('name'),max_length=100)
+    image = models.ImageField(_('image'),upload_to='brand')
+    slug = models.SlugField(null=True,blank=True)
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self , *args ,**kwargs): 
+        self.slug = slugify(self.name ) 
+        super(Brand, self).save( *args ,**kwargs)
+
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User,verbose_name=('user'),related_name='user_review',on_delete=models.SET_NULL,null=True,blank=True)
+    product = models.ForeignKey(Product,verbose_name=('Product'),related_name='product_review',on_delete=models.SET_NULL,null=True,blank=True)
+    review = models.TextField(_('review'),max_length=500)
+    rate = models.IntegerField(_('rate'),validators=[MaxValueValidator(5),MinValueValidator(0)])
+    create_date = models.DateTimeField(_('create_date'),default=timezone.now)
+    
+    def __str__(self):
+        return f"{self.user} = {self.product}"
---
+++ products/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Product , ProductImages, Review ,Brand
+
+
+class ProductList(generic.ListView):
+    model = Product
+    
+    
+    
+class ProductDetail(generic.DetailView):   
+    model = Product
+    
+    
+    
+class BrandList(generic.ListView):
+    model = Brand
+    
+    
+class BrandDetail(generic.DetailView):
+    model = Brand        
+    
+    
+    
+
+# Create your views here.
